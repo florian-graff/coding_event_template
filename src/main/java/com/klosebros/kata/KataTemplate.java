f97f169b5ff18fbd2170d4fb020169cc813d40e9
Jens Golombek <jens.golombek@bertelsmann.de>
2024-02-23 14:25:29 +0100

    WIP

--- src/main/java/com/klosebros/kata/KataTemplate.java
+++ src/main/java/com/klosebros/kata/KataTemplate.java
@@ -1,15 +1,23 @@
 package com.klosebros.kata;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class KataTemplate {
 
     public String bc(String url) {
-        var splitedUrl = Arrays.stream(url.split("/")).toList();
+        var splitedUrl = Arrays.stream(url.split("/")).collect(Collectors.toList());
+
+
+        if (splitedUrl.get(splitedUrl.size()-1).equals("index")) {
+           splitedUrl.remove(splitedUrl.size()-1);
+        }
 
         if (splitedUrl.size() == 1) {
-            return "<span class=\"active\">HOME</span>";
+            return "<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
         }
+
+
 
         return "";
     }
--- src/test/java/com/klosebros/kata/KataTemplateTest.java
+++ src/test/java/com/klosebros/kata/KataTemplateTest.java
@@ -7,18 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class KataTemplateTest {
 
     @Test
-    void emptyBreadcrump() {
+    void emptyBreadcrump1() {
         var kataTemplate = new KataTemplate();
-        var expected ="<span class=\"active\">HOME</span>";
+        var expected ="<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
         var given = "www.example.com/";
         var result = kataTemplate.bc(given);
 
         assertThat(result).isEqualTo(expected);
     }
-    void emptyBreadcrump1() {
+
+    @Test
+    void ignoreIndex() {
         var kataTemplate = new KataTemplate();
-        var expected ="<a href=\"/\">HOME</a> : <a href=\"//\"></a> : <span class=\"active\"></span>";
-        var given = "www.example.com//";
+        var expected ="<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
+        var given = "www.example.com/index";
         var result = kataTemplate.bc(given);
 
         assertThat(result).isEqualTo(expected);
