e212319cabacaae33c828c7d69b0c92ec063b1dd
Walter Wirch <walter.wirch@bertelsmann.de>
2024-02-23 13:59:20 +0100

    Extende first test by paramereized execution

--- src/test/java/com/klosebros/kata/BreadcrumbGeneratorTest.java
+++ src/test/java/com/klosebros/kata/BreadcrumbGeneratorTest.java
@@ -1,18 +1,21 @@
 package com.klosebros.kata;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BreadcrumbGeneratorTest {
-
-    @Test
-    void basicTest(){
+    @ParameterizedTest
+    @ValueSource(strings={"",".php",".htm", ".html", ".asp"})
+    void testBreadcumbGeneration(String ending){
         BreadcrumbGenerator breadcrumbGenerator = new BreadcrumbGenerator();
-        var url = "www.baseurl.com/basic/example";
+        var url = "www.baseurl.com/basic/example" + ending;
         var separator = ":";
         var expected =  "<a href=\"/\">HOME</a> : <a href=\"/basic/\">BASIC</a> : <span class=\"active\">EXAMPLE</span>";
 
         assertThat(breadcrumbGenerator.generateBreadcrumb(url,separator)).isEqualTo(expected);
+
     }
 }
