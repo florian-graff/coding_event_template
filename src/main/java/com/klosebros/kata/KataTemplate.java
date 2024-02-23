949be3dc700f8b62edb087b4caa60727fef686ab
KAEF008 <viktor.kaefer@bertelsmann.de>
2024-02-23 14:40:44 +0100

    Step 1

--- src/main/java/com/klosebros/kata/KataTemplate.java
+++ src/main/java/com/klosebros/kata/KataTemplate.java
@@ -1,22 +1,32 @@
 package com.klosebros.kata;
 
+import io.vavr.collection.List;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class KataTemplate {
 
+    public static final List<String> EXTS = List.of(".html", ".htm", ".asp", ".php");
+
     public String bc(String url) {
         var splitedUrl = Arrays.stream(url.split("/")).collect(Collectors.toList());
 
 
-        if (splitedUrl.get(splitedUrl.size()-1).equals("index")) {
-           splitedUrl.remove(splitedUrl.size()-1);
+        var lastIndex = splitedUrl.size() - 1;
+        EXTS.forEach(ext -> {
+            if (splitedUrl.getLast().contains(ext)) {
+                splitedUrl.set(lastIndex, splitedUrl.getLast().replace(ext, ""));
+            }
+
+        });
+        if (splitedUrl.get(lastIndex).equals("index")) {
+            splitedUrl.remove(lastIndex);
         }
 
         if (splitedUrl.size() == 1) {
             return "<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
         }
-
 
 
         return "";
--- src/test/java/com/klosebros/kata/KataTemplateTest.java
+++ src/test/java/com/klosebros/kata/KataTemplateTest.java
@@ -1,6 +1,8 @@
 package com.klosebros.kata;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,7 +11,7 @@ class KataTemplateTest {
     @Test
     void emptyBreadcrump1() {
         var kataTemplate = new KataTemplate();
-        var expected ="<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
+        var expected = "<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
         var given = "www.example.com/";
         var result = kataTemplate.bc(given);
 
@@ -19,8 +21,19 @@ class KataTemplateTest {
     @Test
     void ignoreIndex() {
         var kataTemplate = new KataTemplate();
-        var expected ="<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
+        var expected = "<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
         var given = "www.example.com/index";
+        var result = kataTemplate.bc(given);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {".html", ".htm", ".asp", ".php"})
+    void ignoreFileExts(String fileExt) {
+        var kataTemplate = new KataTemplate();
+        var expected = "<a href=\"/\">HOME</a> : <a href=\"/\"></a> : <span class=\"active\"></span>";
+        var given = "www.example.com/index" + fileExt;
         var result = kataTemplate.bc(given);
 
         assertThat(result).isEqualTo(expected);
