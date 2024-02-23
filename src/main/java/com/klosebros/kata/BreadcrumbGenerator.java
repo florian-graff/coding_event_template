24221d760a2f62cca5600d3812fa27fcc66ae9f3
WINT143 <magnus.winter@bertelsmann.de>
2024-02-23 14:21:27 +0100

    wip

--- src/main/java/com/klosebros/kata/BreadcrumbGenerator.java
+++ src/main/java/com/klosebros/kata/BreadcrumbGenerator.java
@@ -1,5 +1,7 @@
 package com.klosebros.kata;
 
+import java.util.stream.Stream;
+
 public class BreadcrumbGenerator {
 
     record Url(String base, String path, String target){};
@@ -11,6 +13,16 @@ public class BreadcrumbGenerator {
 
         var template = "<a href=\"/\">HOME</a> %s <a href=\"/%s/\">%s</a> %s <span class=\"active\">%s</span>";
 
-        return template.formatted(separator, urlRecord.path, urlRecord.path.toUpperCase(), separator, urlRecord.target.toUpperCase());
+        return template.formatted(separator, urlRecord.path, urlRecord.path.toUpperCase(), separator, removeEnding(urlRecord.target).toUpperCase());
     }
+
+    private String removeEnding(String target){
+        return Stream.of(".php",".htm", ".html", ".asp")
+                .filter(target::endsWith)
+                .map(s -> target.split("\\.")[0])
+                .findFirst()
+                .orElse(target);
+    }
+
+
 }
