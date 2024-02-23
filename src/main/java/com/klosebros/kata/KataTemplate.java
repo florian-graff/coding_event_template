ecab21b33dd6fc8fd94316fcaa2b9d86843fc5c7
KAEF008 <viktor.kaefer@bertelsmann.de>
2024-02-23 13:52:15 +0100

    Step 1

--- src/main/java/com/klosebros/kata/KataTemplate.java
+++ src/main/java/com/klosebros/kata/KataTemplate.java
@@ -2,4 +2,7 @@ package com.klosebros.kata;
 
 public class KataTemplate {
 
+    public String bc(String url) {
+        return "<a href=\"/\">HOME</a> : <a href=\"//\"></a> : <span class=\"active\"></span>";
+    }
 }
--- src/test/java/com/klosebros/kata/KataTemplateTest.java
+++ src/test/java/com/klosebros/kata/KataTemplateTest.java
@@ -7,7 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class KataTemplateTest {
 
     @Test
-    void deleteThisTest() {
-        assertThat(false).isTrue();
+    void emptyBreadcrump() {
+        var kataTemplate = new KataTemplate();
+        var expected ="<a href=\"/\">HOME</a> : <a href=\"//\"></a> : <span class=\"active\"></span>";
+        var given = "www.example.com//";
+        var result = kataTemplate.bc(given);
+
+        assertThat(result).isEqualTo(expected);
     }
 }
