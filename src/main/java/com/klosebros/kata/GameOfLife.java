24eb259e82c3cf5a793cf0e84b23f67df00705be
Jens Golombek <jens.golombek@bertelsmann.de>
2024-04-12 13:51:29 +0200

    WIP

--- src/main/java/com/klosebros/kata/GameOfLife.java
+++ src/main/java/com/klosebros/kata/GameOfLife.java
@@ -1,12 +1,22 @@
 package com.klosebros.kata;
 
+import java.util.stream.IntStream;
+
 public class GameOfLife {
 
+    public static final String DEADCELL = "0";
+
     public String[][] nextGeneration(String[][] gameMap) {
-        return new String[][]{
-                {"0", "0", "0"},
-                {"0", "0", "0"},
-                {"0", "0", "0"}
-        };
+        int rows = gameMap.length;
+        int columns = gameMap[0].length;
+
+        var nextGen = new String[rows][columns];
+        for (int y = 0; y< rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                nextGen[x][y]= DEADCELL;
+            }
+        }
+
+        return nextGen;
     }
 }
--- src/test/java/com/klosebros/kata/GameOfLifeTest.java
+++ src/test/java/com/klosebros/kata/GameOfLifeTest.java
@@ -3,17 +3,14 @@ package com.klosebros.kata;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.klosebros.kata.GameOfLife.DEADCELL;
+
 
 class GameOfLifeTest {
 
     @Test
     void nothingIsBornOutOfNothing() {
-        String[][] gameMap = {
-                {"0", "0", "0"},
-                {"0", "0", "0"},
-                {"0", "0", "0"}
-        };
+        String[][] gameMap = createEmptyMap(3,3);
 
         GameOfLife gameOfLife = new GameOfLife();
         String[][] nextGeneration = gameOfLife.nextGeneration(gameMap);
@@ -23,16 +20,38 @@ class GameOfLifeTest {
 
     @Test
     void nothingIsBornOutOfNothingForAnySizeMap() {
-        String[][] gameMap = {
-                {"0", "0", "0", "0"},
-                {"0", "0", "0", "0"},
-                {"0", "0", "0", "0"},
-                {"0", "0", "0", "0"}
-        };
+        String[][] gameMap  = createEmptyMap(4,4);
 
         GameOfLife gameOfLife = new GameOfLife();
         String[][] nextGeneration = gameOfLife.nextGeneration(gameMap);
 
         Assertions.assertThat(nextGeneration).isEqualTo(gameMap);
+    }
+
+    @Test
+    void lonelyCellDies() {
+        //Given
+        String[][] gameMap = createEmptyMap(3,3);
+        gameMap[1][1]="X";
+
+        GameOfLife gameOfLife = new GameOfLife();
+
+        //WHEN
+        String[][] nextGeneration = gameOfLife.nextGeneration(gameMap);
+
+        //THEN
+        var emptyMap = createEmptyMap(3,3);
+        Assertions.assertThat(nextGeneration).isEqualTo(emptyMap);
+
+    }
+
+    private static String[][] createEmptyMap(final int rows, final int columns) {
+        var emptyMap = new String[rows][columns];
+        for (int y = 0; y< rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                emptyMap[x][y]= DEADCELL;
+            }
+        }
+        return emptyMap;
     }
 }
