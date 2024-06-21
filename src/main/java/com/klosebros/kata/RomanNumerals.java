abb538cd9872c17e7007d169fb55ea1c24362bad
Emanuel Schulze Pröbsting <emanuel.schulzeproebsting@bertelsmann.de>
2024-06-21 14:04:13 +0200

    wip

--- src/main/java/com/klosebros/kata/RomanNumerals.java
+++ src/main/java/com/klosebros/kata/RomanNumerals.java
@@ -2,34 +2,17 @@ package com.klosebros.kata;
 
 public class RomanNumerals {
 
-    String convert(int i) {
-        String romanNumeral;
-        switch (i) {
-            case 1:
-                romanNumeral = "I";
-                break;
-            case 5:
-                romanNumeral = "V";
-                break;
-            case 10:
-                romanNumeral = "X";
-                break;
-            case 50:
-                romanNumeral = "L";
-                break;
-            case 100:
-                romanNumeral = "C";
-                break;
-            case 500:
-                romanNumeral = "D";
-                break;
-            case 1000:
-                romanNumeral = "M";
-                break;
-            default:
-                throw new RuntimeException();
-        }
-        return romanNumeral;
+    String convert(int number) {
+        return switch (number) {
+            case 1 -> "I";
+            case 5 -> "V";
+            case 10 -> "X";
+            case 50 -> "L";
+            case 100 -> "C";
+            case 500 -> "D";
+            case 1000 -> "M";
+            default -> throw new RuntimeException();
+        };
     }
 
 }
--- src/test/java/com/klosebros/kata/RomanNumeralsTest.java
+++ src/test/java/com/klosebros/kata/RomanNumeralsTest.java
@@ -1,10 +1,18 @@
 package com.klosebros.kata;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanNumeralsTest {
 
@@ -21,6 +29,17 @@ class RomanNumeralsTest {
         var romanNumerals = new RomanNumerals();
         var result = romanNumerals.convert(number);
         assertThat(result).isEqualTo(numeral);
+    }
+
+    @TestFactory
+    Stream<DynamicTest> translateDynamicTestsFromStream() {
+        var numbers = Map.of(1, "I",2, "II",3, "III",4, "III");
+
+        return numbers.entrySet().stream().map(e -> DynamicTest.dynamicTest("Test for " + e.getKey(), () -> {
+            var romanNumerals = new RomanNumerals();
+            var result = romanNumerals.convert(e.getKey());
+            assertThat(result).isEqualTo(e.getValue());
+        }));
     }
 
     
