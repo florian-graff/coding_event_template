1028eb7a3690e97fc930dc5cd84feaded3ef8530
KAEF008 <viktor.kaefer@bertelsmann.de>
2024-09-13 13:40:51 +0200

    First commit TripService

---
+++ src/test/java/com/klosebros/kata/trip/TripServiceTest.java
@@ -0,0 +1,11 @@
+package com.klosebros.kata.trip;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TripServiceTest {
+
+    @Test
+    void getTripsByUser() {
+    }
+}
