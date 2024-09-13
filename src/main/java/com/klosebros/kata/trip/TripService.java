3b9993569bf6ce92eef21a1cf201edab7ed2c363
KAEF008 <viktor.kaefer@bertelsmann.de>
2024-09-13 14:33:30 +0200

    TripService with friends

--- src/main/java/com/klosebros/kata/trip/TripService.java
+++ src/main/java/com/klosebros/kata/trip/TripService.java
@@ -10,32 +10,27 @@ import java.util.List;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
         var loggedUser = getLoggedUser();
-        var isLoggedUserFriend = false;
-		if (loggedUser != null) {
-			for (var friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isLoggedUserFriend = true;
-					break;
-				}
-			}
-			if (isLoggedUserFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+        if (loggedUser == null) {
+            throw new UserNotLoggedInException();
+        }
+        var isLoggedUserFriend = user.getFriends().stream()
+                .anyMatch(friend -> friend.equals(loggedUser));
 
-	List<Trip> findTripsByUser(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+        if (isLoggedUserFriend) {
+            tripList = findTripsByUser(user);
+        }
+        return tripList;
+    }
 
-	User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+    List<Trip> findTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    User getLoggedUser() {
+        return UserSession.getInstance().getLoggedUser();
+    }
 
 }
