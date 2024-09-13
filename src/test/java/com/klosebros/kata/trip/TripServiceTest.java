18c5c1083c631f62e822d7ab7ea3e535eba9641b
Emanuel Schulze Pröbsting <emanuel.schulzeproebsting@bertelsmann.de>
2024-09-13 13:48:58 +0200

    wip

--- src/test/java/com/klosebros/kata/trip/TripServiceTest.java
+++ src/test/java/com/klosebros/kata/trip/TripServiceTest.java
@@ -1,11 +1,34 @@
 package com.klosebros.kata.trip;
 
+import com.klosebros.kata.exception.UserNotLoggedInException;
+import com.klosebros.kata.user.User;
+import com.klosebros.kata.user.UserSession;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-class TripServiceTest {
+public class TripServiceTest {
 
     @Test
-    void getTripsByUser() {
+    void shouldThrowExceptionIfLoggedUserIsNull() {
+        // Given
+        TripService tripService = new TripService();
+        User user = new User();
+
+        // When
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+    }
+}
+
+class TripServiceTestable extends TripService {
+
+    private final User loggedUser;
+
+    TripServiceTestable(User loggedUser) {
+        this.loggedUser = loggedUser;
+    }
+
+    @Override
+    User getLoggedUser() {
+        return loggedUser;
     }
 }
