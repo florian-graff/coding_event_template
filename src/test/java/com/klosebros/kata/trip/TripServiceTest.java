f5618765a2b1847ecb6268061476f9411235f57c
Peter Henrichs <peter.henrichs1@bertelsmann.de>
2024-09-13 14:20:17 +0200

    wip

--- src/test/java/com/klosebros/kata/trip/TripServiceTest.java
+++ src/test/java/com/klosebros/kata/trip/TripServiceTest.java
@@ -2,23 +2,30 @@ package com.klosebros.kata.trip;
 
 import com.klosebros.kata.exception.UserNotLoggedInException;
 import com.klosebros.kata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TripServiceTest {
 
+    TripService tripService;
+
+    @BeforeEach
+    void setUp() {
+        tripService = new TripServiceTestable(new User());
+    }
+
     @Test
     void userWithNoFriendsReturnsEmptyTripList() {
-        TripService tripService = new TripServiceTestable(new User());
         User user = new User();
 
         List<Trip> tripsByUser = tripService.getTripsByUser(user);
 
         assertThat(tripsByUser).isEmpty();
-
     }
 
     @Test
@@ -26,8 +33,6 @@ class TripServiceTest {
         User user = new User();
         user.addFriend(user);
         user.addTrip(new Trip());
-
-        TripService tripService = new TripServiceTestable(user);
 
         List<Trip> tripsByUser = tripService.getTripsByUser(user);
 
@@ -37,13 +42,10 @@ class TripServiceTest {
 
     @Test
     void exceptionThrownIfNoUserLoggedIn() {
-        TripService tripService = new TripServiceTestable(null);
         User user = new User();
 
-        try {
-            tripService.getTripsByUser(user);
-        } catch (Exception e) {
-            assertThat(e).isInstanceOf(UserNotLoggedInException.class);
-        }
+        assertThatThrownBy(() -> tripService.getTripsByUser(user)).isInstanceOf(UserNotLoggedInException.class);
     }
+
+
 }
