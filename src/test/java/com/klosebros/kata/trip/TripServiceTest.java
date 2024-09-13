e7b12de3a6cc25150534d20b644b9c8722f6eba1
broi002 <soenke.broihan@bertelsmann.de>
2024-09-13 14:10:29 +0200

    Test Exeption

--- src/test/java/com/klosebros/kata/trip/TripServiceTest.java
+++ src/test/java/com/klosebros/kata/trip/TripServiceTest.java
@@ -1,17 +1,17 @@
 package com.klosebros.kata.trip;
 
+import com.klosebros.kata.exception.UserNotLoggedInException;
 import com.klosebros.kata.user.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TripServiceTest {
 
     @Test
-    void getTripsByUserNoFriends() {
+    void userWithNoFriendsReturnsEmptyTripList() {
         TripService tripService = new TripServiceTestable(new User());
         User user = new User();
 
@@ -22,14 +22,28 @@ class TripServiceTest {
     }
 
     @Test
-    void getTripsByUserIsFriend() {
+    void userWithFriendsReturnsTripList() {
         User user = new User();
         user.addFriend(user);
+        user.addTrip(new Trip());
+
         TripService tripService = new TripServiceTestable(user);
 
         List<Trip> tripsByUser = tripService.getTripsByUser(user);
 
         assertThat(tripsByUser).isNotEmpty();
 
+    }
+
+    @Test
+    void exceptionThrownIfNoUserLoggedIn() {
+        TripService tripService = new TripServiceTestable(null);
+        User user = new User();
+
+        try {
+            tripService.getTripsByUser(user);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(UserNotLoggedInException.class);
+        }
     }
 }
