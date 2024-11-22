5ed12ed5c338e7330dd4783ee31aecf16a19c4ec
KAEF008 <viktor.kaefer@bertelsmann.de>
2024-11-22 14:14:31 +0100

    TripService with friends

--- src/main/java/com/klosebros/kata/parrot/Parrot.java
+++ src/main/java/com/klosebros/kata/parrot/Parrot.java
@@ -5,9 +5,9 @@ import static com.klosebros.kata.parrot.ParrotTypeEnum.*;
 public abstract class Parrot {
 
     private final ParrotTypeEnum type;
-    private final int numberOfCoconuts;
-    private final double voltage;
-    private final boolean isNailed;
+    protected final int numberOfCoconuts;
+    protected final double voltage;
+    protected final boolean isNailed;
 
     protected Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
@@ -24,19 +24,13 @@ public abstract class Parrot {
         throw new IllegalStateException("Unexpected parrot type");
     }
 
-    public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
-    }
+    public abstract double getSpeed();
 
     protected double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 
-    private double getLoadFactor() {
+    protected double getLoadFactor() {
         return 9.0;
     }
 
