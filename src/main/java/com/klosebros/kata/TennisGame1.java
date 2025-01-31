8fea7e17cf87887211310e7f8546783781da26ec
KAEF008 <viktor.kaefer@bertelsmann.de>
2025-01-31 14:41:12 +0100

    WIP

--- src/main/java/com/klosebros/kata/TennisGame1.java
+++ src/main/java/com/klosebros/kata/TennisGame1.java
@@ -1,5 +1,6 @@
 package com.klosebros.kata;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
@@ -8,8 +9,8 @@ public class TennisGame1 implements TennisGame {
     private int mScore2 = 0;
 
     enum EqualAll {
-        LOVE_ALL(0,"Love-All"),
-        FIFTEEN_ALL(1,"Fifteen-All"),
+        LOVE_ALL(0, "Love-All"),
+        FIFTEEN_ALL(1, "Fifteen-All"),
         THIRTY_ALL(2, "Thirty-All"),
         DEUCE(3, "Deuce");
 
@@ -22,6 +23,9 @@ public class TennisGame1 implements TennisGame {
             this.rank = rank;
         }
 
+        public static EqualAll valueOf(int score) {
+            return Arrays.stream(values()).findFirst().filter(value -> value.rank == score).orElseThrow();
+        }
     }
 
     public void wonPoint(String playerName) {
@@ -32,27 +36,24 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        StringBuilder score ;
+        StringBuilder score;
         int tempScore;
-        if (mScore1 == mScore2)
-        {
+        if (mScore1 == mScore2) {
             score = calculatePlayerScoreByEqualPoints();
-        }
-        else if (mScore1 >=4 || mScore2 >=4)
-        {
+        } else if (mScore1 >= 4 || mScore2 >= 4) {
             var minusResult = mScore1 - mScore2;
-            if (minusResult==1) score = new StringBuilder("Advantage player1");
-            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
-            else if (minusResult>=2) score = new StringBuilder("Win for player1");
+            if (minusResult == 1) score = new StringBuilder("Advantage player1");
+            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
             else score = new StringBuilder("Win for player2");
-        }
-        else
-        {
+        } else {
             score = new StringBuilder();
-            for (var i = 1; i<3; i++)
-            {
-                if (i==1) tempScore = mScore1;
-                else { score.append("-"); tempScore = mScore2;}
+            for (var i = 1; i < 3; i++) {
+                if (i == 1) tempScore = mScore1;
+                else {
+                    score.append("-");
+                    tempScore = mScore2;
+                }
                 switch (tempScore) {
                     case 0 -> score.append("Love");
                     case 1 -> score.append("Fifteen");
@@ -65,15 +66,10 @@ public class TennisGame1 implements TennisGame {
         return score.toString();
     }
 
-    private StringBuilder calculatePdlayerScoreByEqualPoints() {
-        StringBuilder score;
-        var equalAll = EqualAll.valueOf(mScore1);
-        score = new StringBuilder(; {
-            case 0 -> "Love-All";
-            case 1 -> "Fifteen-All";
-            case 2 -> "Thirty-All";
-            default -> "Deuce";
-        });
-        return score;
+    private StringBuilder calculatePlayerScoreByEqualPoints() {
+        if (mScore1 > 2) {
+            return new StringBuilder(EqualAll.DEUCE.value);
+        }
+        return new StringBuilder(EqualAll.values()[mScore1].value);
     }
 }
