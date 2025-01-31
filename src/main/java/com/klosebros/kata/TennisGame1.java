a9547f65212ca927850d8d051ccb3e0ad7e65cc8
Roy Keil <roy.keil@bertelsmann.de>
2025-01-31 14:04:48 +0100

    wip

--- src/main/java/com/klosebros/kata/TennisGame1.java
+++ src/main/java/com/klosebros/kata/TennisGame1.java
@@ -1,77 +1,57 @@
 package com.klosebros.kata;
 
+import java.util.Objects;
+
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1() {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (Objects.equals(playerName, "player1"))
+            mScore1 += 1;
         else
-            m_score2 += 1;
+            mScore2 += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        var score = new StringBuilder();
+        var tempScore=0;
+        if (mScore1 == mScore2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = new StringBuilder(switch (mScore1) {
+                case 0 -> "Love-All";
+                case 1 -> "Fifteen-All";
+                case 2 -> "Thirty-All";
+                default -> "Deuce";
+            });
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (mScore1 >=4 || mScore2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            var minusResult = mScore1 - mScore2;
+            if (minusResult==1) score = new StringBuilder("Advantage player1");
+            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
+            else if (minusResult>=2) score = new StringBuilder("Win for player1");
+            else score = new StringBuilder("Win for player2");
         }
         else
         {
-            for (int i=1; i<3; i++)
+            for (var i = 1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+                if (i==1) tempScore = mScore1;
+                else { score.append("-"); tempScore = mScore2;}
+                switch (tempScore) {
+                    case 0 -> score.append("Love");
+                    case 1 -> score.append("Fifteen");
+                    case 2 -> score.append("Thirty");
+                    case 3 -> score.append("Forty");
+                    default -> throw new IllegalStateException("Unexpected value: " + tempScore);
                 }
             }
         }
-        return score;
+        return score.toString();
     }
 }
