cc73c814946cd33804a7a464a0c9a053e11dc583
Walter Wirch <walter.wirch@bertelsmann.de>
2025-01-31 15:04:58 +0100

    final state

--- src/main/java/com/klosebros/kata/TennisGame1.java
+++ src/main/java/com/klosebros/kata/TennisGame1.java
@@ -1,11 +1,11 @@
 package com.klosebros.kata;
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    private String playerOneName;
+    private String playerTwoName;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,65 +13,80 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(this.playerOneName))
+            playerOneScore += 1;
         else
-            m_score2 += 1;
+            playerTwoScore += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        String score_output = "";
+
+        if (playerOneScore == playerTwoScore) {
+            score_output = determineScoreOutputForDraw(playerOneScore);
+        } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
+            score_output = determineScoreOutputForFinalPhase(playerOneScore, playerTwoScore);
+        } else {
+            score_output = determineScoreOutputOtherwise(playerOneScore, playerTwoScore);
+        }
+        return score_output;
+    }
+
+    private String determineScoreOutputOtherwise(int playerOneScore, int playerTwoScore) {
+        String score_output = "";
+
+        int tempScore = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = playerOneScore;
+            else {
+                score_output += "-";
+                tempScore = playerTwoScore;
+            }
+            switch (tempScore) {
                 case 0:
-                        score = "Love-All";
+                    score_output += "Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score_output += "Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score_output += "Thirty";
                     break;
-                default:
-                        score = "Deuce";
+                case 3:
+                    score_output += "Forty";
                     break;
-                
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return score_output;
+    }
+
+    private String determineScoreOutputForFinalPhase(int playerOneScore, int playerTwoScore) {
+        String score_output;
+        int minusResult = playerOneScore - playerTwoScore;
+        if (minusResult == 1) score_output = "Advantage " + this.playerOneName;
+        else if (minusResult == -1) score_output = "Advantage " + this.playerTwoName;
+        else if (minusResult >= 2) score_output = "Win for " + this.playerOneName;
+        else score_output = "Win for " + this.playerTwoName;
+        return score_output;
+    }
+
+    private String determineScoreOutputForDraw(int score) {
+        String score_output;
+        switch (score) {
+            case 0:
+                score_output = "Love-All";
+                break;
+            case 1:
+                score_output = "Fifteen-All";
+                break;
+            case 2:
+                score_output = "Thirty-All";
+                break;
+            default:
+                score_output = "Deuce";
+                break;
+
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+        return score_output;
     }
 }
