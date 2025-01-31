b026b4d6a053fb097e9773e8350bd08a5c4fd31b
Walter Wirch <walter.wirch@bertelsmann.de>
2025-01-31 15:27:40 +0100

    final state simplified

--- src/main/java/com/klosebros/kata/TennisGame1.java
+++ src/main/java/com/klosebros/kata/TennisGame1.java
@@ -4,8 +4,8 @@ public class TennisGame1 implements TennisGame {
 
     private int playerOneScore = 0;
     private int playerTwoScore = 0;
-    private String playerOneName;
-    private String playerTwoName;
+    private final String playerOneName;
+    private final String playerTwoName;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,80 +13,50 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals(this.playerOneName))
-            playerOneScore += 1;
-        else
-            playerTwoScore += 1;
+        if (playerName.equals(this.playerOneName)) {
+            playerOneScore++;
+        } else {
+            playerTwoScore++;
+        }
     }
 
     public String getScore() {
-        String score_output = "";
-
-        if (playerOneScore == playerTwoScore) {
-            score_output = determineScoreOutputForDraw(playerOneScore);
-        } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
-            score_output = determineScoreOutputForFinalPhase(playerOneScore, playerTwoScore);
-        } else {
-            score_output = determineScoreOutputOtherwise(playerOneScore, playerTwoScore);
-        }
-        return score_output;
+        return playerOneScore == playerTwoScore ? determineScoreOutputForEqualPoints(playerOneScore) :
+                (playerOneScore >= 4 || playerTwoScore >= 4) ? determineScoreOutputForFinalPhase(playerOneScore, playerTwoScore) :
+                        determineScoreOutputOtherwise(playerOneScore, playerTwoScore);
     }
 
     private String determineScoreOutputOtherwise(int playerOneScore, int playerTwoScore) {
-        String score_output = "";
+        return getScoreString(playerOneScore) + "-" + getScoreString(playerTwoScore);
+    }
 
-        int tempScore = 0;
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = playerOneScore;
-            else {
-                score_output += "-";
-                tempScore = playerTwoScore;
-            }
-            switch (tempScore) {
-                case 0:
-                    score_output += "Love";
-                    break;
-                case 1:
-                    score_output += "Fifteen";
-                    break;
-                case 2:
-                    score_output += "Thirty";
-                    break;
-                case 3:
-                    score_output += "Forty";
-                    break;
-            }
-        }
-        return score_output;
+    private String getScoreString(int score) {
+        return switch (score) {
+            case 0 -> "Love";
+            case 1 -> "Fifteen";
+            case 2 -> "Thirty";
+            case 3 -> "Forty";
+            default -> throw new IllegalArgumentException("Unexpected score: " + score);
+        };
     }
 
     private String determineScoreOutputForFinalPhase(int playerOneScore, int playerTwoScore) {
-        String score_output;
         int minusResult = playerOneScore - playerTwoScore;
-        if (minusResult == 1) score_output = "Advantage " + this.playerOneName;
-        else if (minusResult == -1) score_output = "Advantage " + this.playerTwoName;
-        else if (minusResult >= 2) score_output = "Win for " + this.playerOneName;
-        else score_output = "Win for " + this.playerTwoName;
-        return score_output;
+        return switch (minusResult) {
+            case 1 -> "Advantage " + this.playerOneName;
+            case -1 -> "Advantage " + this.playerTwoName;
+            case 2, 3, 4, 5 -> "Win for " + this.playerOneName;
+            case -2, -3, -4, -5 -> "Win for " + this.playerTwoName;
+            default -> throw new IllegalStateException("Unexpected value: " + minusResult);
+        };
     }
 
-    private String determineScoreOutputForDraw(int score) {
-        String score_output;
-        switch (score) {
-            case 0:
-                score_output = "Love-All";
-                break;
-            case 1:
-                score_output = "Fifteen-All";
-                break;
-            case 2:
-                score_output = "Thirty-All";
-                break;
-            default:
-                score_output = "Deuce";
-                break;
-
-        }
-        return score_output;
+    private String determineScoreOutputForEqualPoints(int score) {
+        return switch (score) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
+        };
     }
 }
