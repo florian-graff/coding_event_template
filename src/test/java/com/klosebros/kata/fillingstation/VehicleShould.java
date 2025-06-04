f967b0bb7df949369038d4bb04693ffdd28d6aca
Florian Graff <florian.graff@klosebrothers.de>
2025-06-04 21:15:17 +0200

    filling station

---
+++ src/test/java/com/klosebros/kata/fillingstation/VehicleShould.java
@@ -0,0 +1,45 @@
+package com.klosebros.kata.fillingstation;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VehicleShould {
+  @Test
+  public void start_engine() {
+    Vehicle vehicle = new TestableVehicle();
+
+    vehicle.startEngine();
+
+    assertThat(vehicle.engineIsStarted())
+      .isTrue();
+
+  }
+
+  @Test
+  public void stop_engine() {
+    Vehicle vehicle = new TestableVehicle();
+
+    vehicle.startEngine();
+    vehicle.stopEngine();
+
+    assertThat(vehicle.engineIsStarted())
+      .isFalse();
+  }
+
+
+  public class TestableVehicle extends Vehicle{
+
+    @Override
+    public void fillUpWithFuel() {
+
+    }
+
+    @Override
+    public void chargeBattery() {
+
+    }
+  }
+
+}
