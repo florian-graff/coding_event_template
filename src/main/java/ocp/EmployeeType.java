cec5cd088285d40271255684bc87cccb9a30b495
Florian Graff <florian.graff@klosebrothers.de>
2025-11-28 14:34:02 +0100

    fix

--- src/main/java/ocp/EmployeeType.java
+++ src/main/java/ocp/EmployeeType.java
@@ -1,18 +1,5 @@
 package ocp;
 
 public enum EmployeeType {
-    MANAGER {
-        @Override
-        public int pay(int salary, int bonus) {
-            return salary + bonus;
-        }
-    },
-    ENGINEER {
-        @Override
-        public int pay(int salary, int bonus) {
-            return salary;
-        }
-    };
-
-    public abstract int pay(int salary, int bonus);
+    MANAGER, ENGINEER
 }
