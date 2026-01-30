9a10934ef8982f427216867dab4ce54cd3179b8e
Golombek <jens.golombek@bertelsmann.de>
2026-01-30 14:19:50 +0100

    Wip

--- src/main/java/com/klosebros/kata/PricingEngine.java
+++ src/main/java/com/klosebros/kata/PricingEngine.java
@@ -3,6 +3,13 @@ package com.klosebros.kata;
 public class PricingEngine {
 
     public double calculatePrice(Order order) {
+
+        if (order.destination() == Destination.LOCAL) {
+            return order.basePrice() * 1.19;
+        } else if (order.destination() == Destination.EU) {
+            return order.basePrice() * 1.10;
+        }
+
         return order.basePrice();
     }
 }
--- src/test/java/com/klosebros/kata/PricingEngineTest.java
+++ src/test/java/com/klosebros/kata/PricingEngineTest.java
@@ -28,6 +28,22 @@ class PricingEngineTest {
         var pricingEngine = new PricingEngine();
         var order = new Order(100.0, 2.0, CustomerType.REGULAR, Destination.LOCAL, ShippingType.PICKUP, new Date());
         var price = pricingEngine.calculatePrice(order);
-        assertThat(price).isEqualTo(100.0);
+        assertThat(price).isEqualTo(100.0 * 1.19);
+    }
+
+    @Test
+    void calculateRegularEuOrderByPickup() {
+        var pricingEngine = new PricingEngine();
+        var order = new Order(100.0, 2.0, CustomerType.REGULAR, Destination.EU, ShippingType.PICKUP, new Date());
+        var price = pricingEngine.calculatePrice(order);
+        assertThat(price).isEqualTo(100.0 * 1.10);
+    }
+
+    @Test
+    void calculateRegularInternationalOderByStandard() {
+        var pricingEngine = new PricingEngine();
+        var order = new Order(0.0, 0.0, CustomerType.REGULAR, Destination.INTERNATIONAL, ShippingType.STANDARD, new Date());
+        var price = pricingEngine.calculatePrice(order);
+        assertThat(price).isEqualTo(5);
     }
 }
