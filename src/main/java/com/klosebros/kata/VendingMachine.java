5cdb3d46aea3da412a2102cefd9ee9272ba025f7
Golombek <jens.golombek@bertelsmann.de>
2026-07-10 14:20:02 +0200

    feat: add coin insertion and return functionality to VendingMachine

--- src/main/java/com/klosebros/kata/VendingMachine.java
+++ src/main/java/com/klosebros/kata/VendingMachine.java
@@ -1,15 +1,25 @@
 package com.klosebros.kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
 
     private static final String INSERT_COIN = "GELD EINWERFEN";
     private String display = INSERT_COIN;
+    private List<Coin> coins = new ArrayList<>();
 
     public String getDisplay() {
         return display;
     }
 
     public void insertCoin(Coin coin) {
+        coins.add(coin);
         display = "0,10 EURO EINGEWORFEN";
+    }
+
+    public List<Coin> returnCoins() {
+        display = INSERT_COIN;
+        return coins;
     }
 }
--- src/test/java/com/klosebros/kata/VendingMachineTest.java
+++ src/test/java/com/klosebros/kata/VendingMachineTest.java
@@ -20,4 +20,14 @@ class VendingMachineTest {
 
         assertThat(vendingMachine.getDisplay()).isEqualTo("0,10 EURO EINGEWORFEN");
     }
+
+    @Test
+    void vendingMachine_returnCoin() {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.insertCoin(Coin.ZEHN_CENT);
+        var coins = vendingMachine.returnCoins();
+
+        assertThat(coins).containsExactly(Coin.ZEHN_CENT);
+        assertThat(vendingMachine.getDisplay()).isEqualTo("GELD EINWERFEN");
+    }
 }
